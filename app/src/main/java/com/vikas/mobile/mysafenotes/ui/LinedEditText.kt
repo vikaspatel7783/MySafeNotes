9dd1f0b1a7a66f92daf578f954304deebaa89821
Vikas Patel <vikaspatel7783@gmail.com>
2021-12-29 15:58:21 +0530

    Lined effect added to edit text

---
+++ app/src/main/java/com/vikas/mobile/mysafenotes/ui/LinedEditText.kt
@@ -0,0 +1,41 @@
+package com.vikas.mobile.mysafenotes.ui
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Rect
+import android.util.AttributeSet
+import com.vikas.mobile.mysafenotes.R
+
+class LinedEditText(context: Context, attrs: AttributeSet?) : androidx.appcompat.widget.AppCompatEditText(context, attrs) {
+    private val mRect: Rect = Rect()
+    private val mPaint: Paint = Paint()
+
+    override fun onDraw(canvas: Canvas) {
+//        var count = lineCount;
+        val height = height
+        val lineHeight = lineHeight
+        var count = height / lineHeight
+        if (lineCount > count) count = lineCount //for long text with scrolling
+        val r = mRect
+        val paint = mPaint
+        var baseline = getLineBounds(0, r) //first line
+        for (i in 0 until count) {
+            canvas.drawLine(
+                    r.left.toFloat(),
+                    (baseline + 1).toFloat(),
+                    r.right.toFloat(),
+                    (baseline + 1).toFloat(), paint)
+            baseline += lineHeight //next line
+        }
+        super.onDraw(canvas)
+    }
+
+    // we need this constructor for LayoutInflater
+    init {
+        mPaint.style = Paint.Style.FILL_AND_STROKE
+        mPaint.color = context.getColor(R.color.note_content)
+//        mPaint.color = Color.parseColor("#C0C0C0") //SET YOUR OWN COLOR HERE
+    }
+}
