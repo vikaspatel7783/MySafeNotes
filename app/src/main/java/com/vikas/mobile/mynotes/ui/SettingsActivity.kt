032be5db0f570bf0b322c86bded4e5fb84e146e6
Vikas Patel <vikaspatel7783@gmail.com>
2022-01-03 14:10:00 +0530

    Device secure check in SettingsActivity.kt for toggle ON. few UI customization

--- app/src/main/java/com/vikas/mobile/mynotes/ui/SettingsActivity.kt
+++ app/src/main/java/com/vikas/mobile/mynotes/ui/SettingsActivity.kt
@@ -1,13 +1,14 @@
 package com.vikas.mobile.mynotes.ui
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.ToggleButton
+import android.view.MenuItem
+import android.widget.*
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.vikas.mobile.authandcrypto.BiometricPromptUtils
 import com.vikas.mobile.mynotes.BuildConfig
 import com.vikas.mobile.mynotes.R
 import com.vikas.mobile.mynotes.data.entity.Setting
@@ -29,26 +30,67 @@ class SettingsActivity : AppCompatActivity() {
                 Setting.interpretAuthSettingValue(authSetting.value)
         })
 
-        findViewById<TextView>(R.id.value_settings_version).text = BuildConfig.VERSION_NAME
+        assignVersion()
 
+        onEmailClick()
+
+        onAuthToggle()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun onAuthToggle() {
+        getAuthSwitchToggleButton().setOnCheckedChangeListener { toggleButtonView, isChecked ->
+            run {
+                if (isChecked) {
+                    if (!BiometricPromptUtils.isDeviceSecured(this)) {
+                        handleIfDeviceNotSecured(toggleButtonView)
+                        return@run
+                    }
+                }
+                settingsViewModel.updateAuthSetting(
+                    Setting(
+                        name = Setting.AUTH_SETTINGS_NAME,
+                        value = if (isChecked) Setting.AUTH_ON else Setting.AUTH_OFF
+                    )
+                )
+            }
+        }
+    }
+
+    private fun handleIfDeviceNotSecured(toggleButtonView: CompoundButton) {
+        AlertDialog.Builder(this)
+            .setTitle(getString(R.string.error_no_authentication_title))
+            .setMessage(getString(R.string.error_no_authentication_method))
+            .setCancelable(false)
+            .setPositiveButton(android.R.string.ok) { dg: DialogInterface, _: Int ->
+                toggleButtonView.isChecked = false
+                dg.dismiss()
+            }
+            .show()
+    }
+
+    private fun onEmailClick() {
         findViewById<ImageView>(R.id.ic_developer_email).setOnClickListener {
             val email = Intent(Intent.ACTION_SEND)
-            email.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.settings_developer_email)))
+            email.putExtra(
+                Intent.EXTRA_EMAIL,
+                arrayOf(getString(R.string.settings_developer_email))
+            )
             email.putExtra(Intent.EXTRA_SUBJECT, "MyNotes")
             email.type = "message/rfc822"
             startActivity(Intent.createChooser(email, "Choose an Email client :"))
         }
+    }
 
-        getAuthSwitchToggleButton().setOnCheckedChangeListener { buttonView, isChecked ->
-            run {
-                    settingsViewModel.updateAuthSetting(
-                        Setting(
-                            name = Setting.AUTH_SETTINGS_NAME,
-                            value = if (isChecked) Setting.AUTH_ON else Setting.AUTH_OFF
-                        )
-                    )
-            }
-        }
+    private fun assignVersion() {
+        findViewById<TextView>(R.id.value_settings_version).text = BuildConfig.VERSION_NAME
     }
 
     private fun getAuthSwitchToggleButton(): ToggleButton {
