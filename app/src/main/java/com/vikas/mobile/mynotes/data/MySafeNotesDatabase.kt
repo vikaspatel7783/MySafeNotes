6800a61d4efc23b1f6776a5f15c119e231ba71db
Vikas Patel <vikaspatel7783@gmail.com>
2022-01-04 21:18:51 +0530

    appId changed. removed worker

--- app/src/main/java/com/vikas/mobile/mynotes/data/MySafeNotesDatabase.kt
+++ app/src/main/java/com/vikas/mobile/mynotes/data/MySafeNotesDatabase.kt
@@ -44,8 +44,6 @@ abstract class MySafeNotesDatabase: RoomDatabase() {
                         override fun onCreate(db: SupportSQLiteDatabase) {
                             super.onCreate(db)
 //                            CoroutineScope(Dispatchers.IO).launch {}
-//                            val request = OneTimeWorkRequestBuilder<DefaultNotesSeedWorker>().build()
-//                            WorkManager.getInstance(context).enqueue(request)
                             val request = OneTimeWorkRequestBuilder<DefaultSettingsWorker>().build()
                             WorkManager.getInstance(context).enqueue(request)
                         }
